405d8c36a1d7637c6be76dc8106451fd1e80e9b2
mutalip <jokermt2357@gmail.com>
2020-05-19 17:52:59 +0600

    Update cors

--- app.js
+++ app.js
@@ -4,6 +4,11 @@ const app = express();
 const route = express.Router();
 const config  = require('dotenv').config();
 app.use(bodyParser.json());
+app.use(function(req, res, next) {
+    res.header("Access-Control-Allow-Origin", "*"); 
+    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+    next();
+});
 app.get('/', function (req, res) {
     res.send(config.parsed.APP_PORT);
 });
